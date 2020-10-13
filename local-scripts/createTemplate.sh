3facb52cd1a2d70f202b8bee4dbbdd937519cd32
Florian Kräutli <fkraeutli@mpiwg-berlin.mpg.de>
2020-10-13 15:08:15 +0200

    rename app

--- local-scripts/createTemplate.sh
+++ local-scripts/createTemplate.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 source .env
 
-CURRENT_DIR=pwd
-
 base="http%3A%2F%2Frs.example.org%2F"
 app="example"
-
-cd $REPO_DIR
 
 type=$1
 identifier=$2
@@ -34,6 +30,6 @@ esac
 
 filename="$prefix$identifier.html"
 
-touch "platform/apps/$app/data/templates/$filename"
+touch "$REPO_DIR/platform/apps/$app/data/templates/$filename"
 
 echo "Created new template $filename"
